ed549e074fff9eb872192719355cfe8815ad058e
MSI\AV <varismarcell@gmail.com>
2022-12-07 23:48:42 -0500

    edited some basic css stuff and set up some more with API calls and layout of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import './App.css'
 function App() {
     const [data, setData] = useState({reactors: [], plant_name: ""})
 
+    const apiKey = "eb800069a59bb6c8" // My (Sam's) API Key
+
     const fetchData = async () => {
-        const rawData = await fetch("https://nuclear.dacoder.io/reactors?apiKey=eb800069a59bb6c8")
+        const rawData = await fetch(`https://nuclear.dacoder.io/reactors?apiKey=${apiKey}`)
         const jsonData = await rawData.json()
+        setData(jsonData)
+    }
+
+    const changeReactorName = async (name) => {
+        const newData = await fetch(`https://nuclear.dacoder.io/reactors?apiKey=${apiKey}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "PUT",
+            body: JSON.stringify({ reactorName: name })
+        })
+        const jsonData = await newData.json()
         setData(jsonData)
     }
 
     useEffect(() => {
         fetchData()
     }, [])
-
-    console.log(data)
 
     return (
         <div className="App">
@@ -24,7 +37,10 @@ function App() {
                 return <p>{ reactor.name }</p>
                 })}
             </div>
-            
+            <div className="logs">
+                Console
+            </div>
+
         </div>
     )
 }
