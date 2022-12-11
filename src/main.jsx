efaac32e3d4c521438f6789ca19815469fbead94
Adrian\Adrian Morales <adrianm@southen.edu>
2022-12-11 00:34:18 -0500

    Fixed Merge Conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import TopBar from './TopBar'
+import SideBar from './components/SideBar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='wrapper'>
+      <SideBar />
+      <rightSideBar />
+      <TopBar />
+      <App />
+      
+    </div>
   </React.StrictMode>
 )
